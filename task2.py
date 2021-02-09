4a6e584191342090cce865af1cb850aa718fd033
Leonid Popryho <18726515+popryho@users.noreply.github.com>
2021-02-09 23:29:02 +0200

    Code simplification.

--- task2.py
+++ task2.py
@@ -1,25 +1,13 @@
-from datetime import datetime
-
 import pandas as pd
 
 data = pd.read_json(r'output.json')
+# necessary columns
 fields = ['title', 'link', 'summary', 'published']
 
+# apply to_datetime function along published axis in data dataframe
+data['published'] = pd.to_datetime(data['published'])
 
-def string_to_date(x):
-    if x is None:
-        return None
-    if len(x) == 31:
-        return datetime.strptime(x, '%a, %d %b %Y %H:%M:%S %z')
-    if len(x) == 20:
-        return datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ')
-    if len(x) == 25:
-        return datetime.strptime(x, '%Y-%m-%dT%H:%M:%S%z')
-
-
-data['published'] = data['published'].apply(string_to_date)
-
-print(data['published'])
+# output the result on all entries on necessary columns to output_task2.json file
 with open("output_task2.json", 'w') as outfile:
     data[fields].to_json(outfile, orient="records")
 
